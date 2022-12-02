38603fe790a51296de3fb0b7fb817b5bd4a35aa4
Iachini <iachiniarmndo@gmail.com>
2022-12-02 17:38:12 +0000

    commit tree d9507590fd0a62193c54ca2f497ab12d6599b36b

---
+++ index.js
@@ -0,0 +1,11 @@
+//describe('introduction(name)', function() {
+  //  it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
+    //    expect(introduction("Aki")).toEqual("Hi, my name is Aki.");
+      //  expect(introduction("Samip")).toEqual("Hi, my name is Samip.");
+     // }
+
+
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+introduction("Aki");
